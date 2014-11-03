40a0d236e5fdd0b6ef4e2a7c80c26c298b72e991
kmerrell <kmerrell@gmail.com>
2014-11-02 20:48:52 -0600

    filename changes (after git add)

---
+++ d83/core/common.service.js
@@ -0,0 +1,13 @@
+angular
+    .module('d83.core')
+    .factory('common', ['$q', '$rootScope', '$timeout', 'logger', common]);
+
+function common($q, $rootScope, $timeout) {
+    var service = {
+        // common angular dependencies
+        $q: $q,
+        $timeout: $timeout
+
+    };
+    return service;
+}
